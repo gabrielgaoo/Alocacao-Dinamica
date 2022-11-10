26d56f01e3c49e5734ad513f90649e357c41c11e
gabriel_7ga <106560166+gabrielgaoo@users.noreply.github.com>
2022-11-10 15:01:45 -0300

    comentários
    Adicionando comentários sobre a função malloc

--- malloc.c
+++ malloc.c
@@ -6,15 +6,22 @@ int main(void) {
   printf("digite o tamanho que sera alocado\n");
   scanf("%d",&TAM);
     p = (int*) malloc( TAM * sizeof(int));
-    if(p == NULL){
+    //Sizeof(tipo-de-dado) a função retorna o tamanho em byte do tipo que foi solicitado
+    
+    // malloc(tamanho*sizeof(tipo-de-dado)) a malloc difine o espaço em que os dados serão guardados e 
+    //cria um ponteiro generico que aponta para onde foi alocado os dados.
+    
+    //(int*)  esse código fará a conversão do tipo genérico do ponteiro para o tipo desejado. 
+    
+    if(p == NULL){ // verificando se não houve erro na alocação de memoria, se o ponteiro for = NULL não foi possivel alocar memoria. 
       printf("Erro!\n");
     }
    else{
-     for(i=0;i<TAM;i++){
+     for(i=0;i<TAM;i++){// criando laço para implementar os dados na memoria.
        printf("digite os dados\n");
        scanf("%d",&p[i]);
      }
-    for(i=0;i<TAM;i++){
+    for(i=0;i<TAM;i++){// Imprimindo o resultado.
       printf("Impressao: %d\n",p[i]);
     }
      }

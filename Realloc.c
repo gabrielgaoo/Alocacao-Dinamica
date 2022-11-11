f5b3a793e43edd638d3dade370f70ee39fc6553f
gabriel_7ga <106560166+gabrielgaoo@users.noreply.github.com>
2022-11-11 09:21:42 -0300

    Realloc
    Criando a função realloc

---
+++ Realloc.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+void realoca(int TAM);
+int main(void) {
+  int *p;
+  int TAM, i;
+  printf("digite o tamanho que sera alocado\n");
+  scanf("%d", &TAM);
+  p = (int *)calloc(TAM, sizeof(int));
+  if (p == NULL) {
+    printf("Erro!\n");
+  } else {
+    for (i = 0; i < TAM; i++) {
+      printf("digite os dados\n");
+      scanf("%d", &p[i]);
+    }
+    for (i = 0; i < TAM; i++) {
+      printf("Impressao: %d\n", p[i]);
+    }
+  }
+  // aumentando o tamanho da memoria utilizando o realloc()
+  printf("digite o tamanho que sera realocado\n");
+  scanf("%d", &TAM);
+  realoca(TAM);
+
+  return 0;
+}
+void realoca(int TAM) {
+
+  int *p;
+  p = (int *)realloc(*p, TAM * sizeof(int));
+  if (p == NULL) {
+    printf("Erro!\n");
+  }
+  for (int i = 0; i < TAM; i++) {
+    printf("digite os valores atualizados\n");
+    scanf("%d", &p[i]);
+  }
+    for (int i = 0; i < TAM; i++) {
+      printf("Impressao: %d\n", p[i]);
+    }
+}
